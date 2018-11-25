07cf969390f5c10f926c4a05029ce3050589df07
DESKTOP-V1SFFS6\Dominik <maciejewskidominik97@gmail.com>
2018-11-25 23:56:05 +0100

    layout 3.1 zrobiony

--- app/src/main/java/com/example/dominik/uberpaczka/Rejestracja.java
+++ app/src/main/java/com/example/dominik/uberpaczka/Rejestracja.java
@@ -2,6 +2,8 @@ package com.example.dominik.uberpaczka;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Rejestracja extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Rejestracja extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rejestracja);
+
+        final Button button = findViewById(R.id.registernew);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                android.content.Intent myIntent = new android.content.Intent(v.getContext(), MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
