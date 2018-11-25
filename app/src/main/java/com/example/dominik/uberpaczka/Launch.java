c58582cba6d1bd80eaed596363a47b8992e9879a
DESKTOP-V1SFFS6\Dominik <maciejewskidominik97@gmail.com>
2018-11-25 22:47:45 +0100

    zrobione logo zrobiony layout 2.0 zrobiony layout 1.0

--- app/src/main/java/com/example/dominik/uberpaczka/Launch.java
+++ app/src/main/java/com/example/dominik/uberpaczka/Launch.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Launch extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +15,10 @@ public class Launch extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(Launch.this, Launch.class);
+                Intent homeIntent = new Intent(Launch.this, Logowanie.class);
                 startActivity(homeIntent);
                 finish();
             }
         }, SPLASH_TIME_OUT);
-
     }
 }
--- app/src/main/java/com/example/dominik/uberpaczka/Logowanie.java
+++ app/src/main/java/com/example/dominik/uberpaczka/Logowanie.java
@@ -2,6 +2,8 @@ package com.example.dominik.uberpaczka;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Logowanie extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class Logowanie extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logowanie);
+        final Button button = findViewById(R.id.register);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                android.content.Intent myIntent = new android.content.Intent(v.getContext(), Rejestracja.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
