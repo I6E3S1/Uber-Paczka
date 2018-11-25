9072aa05bc39941103f46b5d563e7d2f86822b21
DESKTOP-V1SFFS6\Dominik <maciejewskidominik97@gmail.com>
2018-11-25 21:56:43 +0100

    layout 2 bez logo layout 3.1 bez scroll view

---
+++ app/src/main/java/com/example/dominik/uberpaczka/Launch.java
@@ -0,0 +1,25 @@
+package com.example.dominik.uberpaczka;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Launch extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(Launch.this, Launch.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+}
