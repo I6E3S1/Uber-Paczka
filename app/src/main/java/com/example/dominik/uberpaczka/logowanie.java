2e26c2e381adae35783a48957ab499a1cf8e2a9f
DESKTOP-V1SFFS6\Dominik <maciejewskidominik97@gmail.com>
2018-11-25 01:25:55 +0100

    initial

---
+++ app/src/main/java/com/example/dominik/uberpaczka/logowanie.java
@@ -0,0 +1,13 @@
+package com.example.dominik.uberpaczka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class logowanie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logowanie);
+    }
+}
