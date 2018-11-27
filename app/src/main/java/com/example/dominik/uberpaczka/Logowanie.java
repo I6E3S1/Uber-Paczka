43c526e7781c25f0d69a15c9ba16cbcbf3a80d1a
DESKTOP-V1SFFS6\Dominik <maciejewskidominik97@gmail.com>
2018-11-27 01:52:46 +0100

    podłączony firebase z autoryzacją działa tworzenie nowych kont działa logowanie - trzeba zabezpieczyć puste edit texty

--- app/src/main/java/com/example/dominik/uberpaczka/Logowanie.java
+++ app/src/main/java/com/example/dominik/uberpaczka/Logowanie.java
@@ -1,22 +1,85 @@
 package com.example.dominik.uberpaczka;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Logowanie extends AppCompatActivity {
+
+    private static final String TAG = "logowanie_email";
+    private FirebaseAuth mAuth;
+
+    EditText email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logowanie);
-        final Button button = findViewById(R.id.register);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        mAuth = FirebaseAuth.getInstance();
+
+        final Button subbutton = findViewById(R.id.submit);
+
+        subbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                signin();
+                android.content.Intent myIntent = new android.content.Intent(v.getContext(), MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        final Button rejbutton = findViewById(R.id.register);
+
+        rejbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 android.content.Intent myIntent = new android.content.Intent(v.getContext(), Rejestracja.class);
                 startActivity(myIntent);
             }
         });
     }
+
+    void signin(){
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        String emailS = email.getText().toString();
+        String passwordS = password.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(emailS, passwordS)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.v(TAG, "sukces");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "blad", task.getException());
+                            Toast.makeText(Logowanie.this, "Błąd logowania.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/dominik/uberpaczka/Rejestracja.java
+++ app/src/main/java/com/example/dominik/uberpaczka/Rejestracja.java
@@ -1,11 +1,35 @@
 package com.example.dominik.uberpaczka;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Rejestracja extends AppCompatActivity {
+
+    private static final String TAG = "rejestracja";
+    private FirebaseAuth mAuth;
+
+    EditText name,newemail,newpassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,11 +37,78 @@ public class Rejestracja extends AppCompatActivity {
         setContentView(R.layout.activity_rejestracja);
 
         final Button button = findViewById(R.id.registernew);
+
+        name = findViewById(R.id.name);
+
+        mAuth = FirebaseAuth.getInstance();
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                register();
                 android.content.Intent myIntent = new android.content.Intent(v.getContext(), MainActivity.class);
                 startActivity(myIntent);
             }
         });
     }
+
+    void register(){
+
+        newemail = findViewById(R.id.email);
+        newpassword = findViewById(R.id.newpassword);
+        String email = newemail.getText().toString();
+        String password = newpassword.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(Rejestracja.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+/*
+    void newUser(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        // [START add_ada_lovelace]
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+
+        user.put("email", newemail.getText().toString());
+
+        user.put("password", newpassword.getText().toString());
+
+        user.put("name", name.getText().toString());
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+        // [END add_ada_lovelace]
+    }*/
+
 }
